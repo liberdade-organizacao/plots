3492f5fedf073664a2bb510b4c18181ef14c3776
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-06-05 20:32:32 -0300

    Laying out scaling step

--- plots.js
+++ plots.js
@@ -19,6 +19,28 @@ function range(from, to, steps) {
     return outlet;
 }
 
+function scale(x, y, w, h) {
+    var outlet = [];
+    var dy = max(y) - min(y);
+    var dx = x[x.length-1] - x[0];
+
+    for (var i = 0; i < x.length; i++) {
+        // TODO turn these points into canvas points
+        o[i] = {
+            x: 0,
+            y: 0
+        };
+    }
+
+    return outlet;
+}
+
 function plot(where, x, f) {
     // TODO Implement me!
+    var canvas = document.getElementById(where);
+    var f_x = apply(f, x);
+    var width = canvas.width;
+    var height = canvas.height;
+    var points = scale(x, f_x, width, height);
+    debugger;
 }
