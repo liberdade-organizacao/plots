d9645378d067727556871624d785e6d36e0cd676
Cris Silva Jr <ishiikurisu@yahoo.com>
2019-06-05 21:31:36 -0300

    Adding more documentation

--- plots.js
+++ plots.js
@@ -1,5 +1,12 @@
 var plots = { };
 
+/**
+ * This implements a wrapper to compare all elements in an array to find a
+ * winner
+ * @param v the array to be analyzed
+ * @param f the comparing function
+ * @returns the winner
+ */
 plots.compare_pairwise = function(v, f) {
     var o = v[0];
 
@@ -10,23 +17,48 @@ plots.compare_pairwise = function(v, f) {
     return o;
 }
 
+/**
+ * finds the biggest number in an array
+ * @param v the array
+ * @returns the biggest element
+ */
 plots.max = function(v) {
     return plots.compare_pairwise(v, function(a, b) {
         return a > b;
     });
 }
 
+/**
+ * finds the smallest number in an array
+ * @param v the array
+ * @returns the smallest element
+ */
 plots.min = function(v) {
     return plots.compare_pairwise(v, function(a, b) {
         return a < b;
     });
 }
 
+/**
+ * maps a value in a range to another
+ * @param x the value tp be mapped
+ * @param in_min the lower bound of the input range
+ * @param in_min the upper bound of the input range
+ * @param in_min the lower bound of the output range
+ * @param in_min the upper bound of the output range
+ * @returns the mapped value
+ */
 plots.map = function(x, in_min, in_max, out_min, out_max) {
     // thanks arduino
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
+/**
+ * applies a function to every value in an array
+ * @param f the function
+ * @param x the array
+ * @returns the mapped array
+ */
 plots.apply = function(f, x) {
     var o = [];
     for (var i = 0; i < x.length; i++) {
@@ -35,6 +67,13 @@ plots.apply = function(f, x) {
     return o;
 }
 
+/**
+ * creates an array from a range between 2 numbers
+ * @param from the lower bound of the range
+ * @param to the upper bound of the range
+ * @param steps the size of the output
+ * @returns an array with numbers between the specified range
+ */
 plots.range = function(from, to, steps) {
     var outlet = [];
     var step = (to-from) / steps;
@@ -48,6 +87,15 @@ plots.range = function(from, to, steps) {
     return outlet;
 }
 
+/**
+ * zips two vectors while mapping their values as coordinates in a plane
+ * @param x the array representing the values on the x axis
+ * @param y the array representing the values on the y axis
+ * @param w the plane width
+ * @param h the plane height
+ * @returns an array with objects contain x and y as the coordinates in the
+ *          specified plane
+ */
 plots.scale = function(x, y, w, h) {
     var outlet = [];
     var min_x = plots.min(x);
@@ -65,6 +113,12 @@ plots.scale = function(x, y, w, h) {
     return outlet;
 }
 
+/**
+ * draws the plot of a function in a HTML canvas
+ * @param where the id of the HTML canvas element
+ * @param x the function input
+ * @param f the function
+ */
 plots.plot = function(where, x, f) {
     var canvas = document.getElementById(where);
     var points = plots.scale(x, plots.apply(f, x), canvas.width, canvas.height);
